06e5be87a7fc0b441f940ed29f8e77d0b5b6440e
YuanChunhai <YuanChunhai>
2022-05-20 11:26:29 +0800

    @EnableTsf包含@EnableTsfAuth

--- provider-demo/src/main/java/com/example/provider/ProviderApplication.java
+++ provider-demo/src/main/java/com/example/provider/ProviderApplication.java
@@ -3,11 +3,9 @@ package com.example.provider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.tsf.annotation.EnableTsf;
-import org.springframework.tsf.auth.annotation.EnableTsfAuth;
 
 @EnableTsf
 @SpringBootApplication
-@EnableTsfAuth
 public class ProviderApplication {
 
     public static void main(String[] args) {
